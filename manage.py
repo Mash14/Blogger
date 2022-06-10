340784d62c78690a8c9ec342ebe436fd02ae7957
Mash14 <mashalonzo741@gmail.com>
2022-06-10 08:54:48 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import create_app,db
 # Server class to launch the server
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
-from app.models import User
+from app.models import User,Blog,Comment
 
 # Creating app instance
 app = create_app('development')
@@ -30,7 +30,7 @@ def test():
 # Shell Context
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User)
+    return dict(app = app, db = db, User = User,Blog = Blog,Comment = Comment)
 
 if __name__ == '__main__':
     manager.run()
